49e9977254777fb29d431516e3d37cd6aa1e2872
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-04-11 12:58:01 -0500

    next and back buttons working, also some style added

--- index.js
+++ index.js
@@ -50,10 +50,14 @@ const quesionnaire = [
 ]
 
 window.addEventListener('load', function(){
-    var reviewOptions = document.querySelector('.display--items'),
+    var  userPrevResponse = null,
         title = document.querySelector('.title'),
+        progressBarStep = 0,
         progressBarSteps = [],
-        userPrevResponse = null,
+        
+        reviewOptions = document.querySelector('.display--items'),
+        userAnswer = document.querySelector('.user--answer'),
+        navItems = [],
         options = [],
         optionsImgs = [],
         optionsPs = [];
@@ -62,7 +66,10 @@ window.addEventListener('load', function(){
         //get previous answers
         fetch(`https://us-central1-quote-db5a2.cloudfunctions.net/widgets/getUserDocument?email=leidercalvo@gmail.com`)
         .then( response => response.json() )
-        .then( r => userPrevResponse = r );
+        .then( r => { 
+            userPrevResponse = r;
+            next(1);
+        });
             
         //create OptionsFields
         [... new Array(4)].forEach( x => {
@@ -91,6 +98,7 @@ window.addEventListener('load', function(){
             navItemFormat.innerHTML = `<img src="./imgs/${(i+1)+''+2}.svg" alt="img">`;
             navItemFormat.addEventListener('click', ()=>next(i+1));
             document.querySelector('.nav--items').appendChild( navItemFormat );
+            navItems.push( navItemFormat );
 
             //create progress bar steps
             var progressStep = document.createElement( 'div' );
@@ -100,33 +108,62 @@ window.addEventListener('load', function(){
             progressBarSteps.push( progressStep );
         });
 
-        next(1);
     } )();
 
 
     function next(pos) {
-        progressBarSteps.forEach( p => {
+        progressBarSteps.forEach( (p, i) => {
             p.classList.remove("is-active");
             p.classList.remove("is-complete");
+            navItems[i].classList.remove("selec");
         });
 
         [... new Array(pos)].forEach( (step, i) => {
             if(i === pos-1){
                 progressBarSteps[i].classList.add("is-active");
+                navItems[i].classList.add("selec");
                 return;
             }
             progressBarSteps[i].classList.add("is-complete");
         });
-
+        progressBarStep = pos;
         updateInfo(pos-1);
     }
 
     function updateInfo(pos) {
         title.innerHTML = quesionnaire[pos].question;
+        userAnswer.innerHTML = ObjectToString(userPrevResponse.answers[pos]);
+        
         quesionnaire[pos].options.forEach( (option, i) => {
+            options[i].classList.remove('selected');
+            checkOptionSelected(pos, option, options[i]);
             if(option !== '') optionsImgs[i].src = './imgs/'+(pos+1)+''+(i+1)+'.svg';
             option === '' ? options[i].style.display = 'none' : options[i].style.display = 'flex';
             optionsPs[i].innerHTML = option;
         });
     }
+
+    function checkOptionSelected(pos, optionText, optionDiv) {
+        const response = userPrevResponse.answers[pos];
+        let isSelected = typeof response === 'string' ? optionText === response : Object.values(response).some( r => r === optionText);
+        isSelected && optionDiv.classList.add('selected');
+    }
+
+    function ObjectToString(object) {
+        if(typeof object === 'string') return object;
+
+        var text = [];
+        for (const prop in object) {
+            object.hasOwnProperty(prop) && object[prop] !== '' && text.push(object[prop])
+        }
+        return text.join(' -- ');
+    }
+
+    document.querySelector('.next').addEventListener('click', ()=> {
+        if(progressBarStep+1 <= userPrevResponse.answers.length) next(progressBarStep+1);
+    });
+
+    document.querySelector('.back').addEventListener('click', ()=> {
+        if(progressBarStep-1 > 0) next(progressBarStep-1);
+    });
 });
