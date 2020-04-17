8b3c0b5c1aa1869449a7e76b506821918ea33601
LeiderCalvo <leiderstivencalvogomez@hotmail.com>
2020-04-17 18:04:27 -0500

    inputs loaders are done

--- generalGuideline.js
+++ generalGuideline.js
@@ -3,31 +3,55 @@ const imageTypes = ['image/gif', 'image/jpeg', 'image/png'];
 window.addEventListener('load', function(){
     let response = [{},{},{},{},{},{}];
 
-    document.querySelectorAll('.ta').forEach( (textArea, i) => 
+    document.querySelectorAll('.ta').forEach( textArea => 
         textArea.addEventListener('change', e => {
-            var pos = parseInt(textArea.dataset.pos),
+            let pos = parseInt(textArea.dataset.pos),
                 prop = textArea.dataset.prop? textArea.dataset.prop : 'textArea';
             response[pos][prop] = e.target.value;
             this.console.log(response);
         })
     );
-/*
-    inp_gguideline.onchange = e => {
-        var acceptedFiles = e.target.files;
 
-        var reader = new FileReader();
-        reader.onload = e => {
-            response.file = e.target.result;
-            this.console.log(acceptedFiles);
+    let files_loaded = document.querySelectorAll('.files_loaded'),
+        file_loaders = document.querySelectorAll('.file--loader');
 
-            var doc = document.createElement( 'div' );
-            doc.classList.add('row');
-            doc.classList.add('document');
-            doc.innerHTML = `<img src=${imageTypes.includes(acceptedFiles[0].type)? e.target.result :'./imgs/doc.svg'} alt="doc">
-            <div><h5>${acceptedFiles[0].name}</h5><p>Estado del doc</p></div>`;
-            files_loaded_gguideline.appendChild(doc);
+    document.querySelectorAll('.inp_loader').forEach( (inp_file, i) => {
+        let pos = parseInt(inp_file.dataset.pos),
+            prop = inp_file.dataset.prop? inp_file.dataset.prop : 'inpFile',
+            max = parseInt(inp_file.dataset.max);
+
+            
+        inp_file.onchange = e => {
+            let acceptedFiles = e.target.files;
+            var reader = new FileReader();
+
+            let id = Math.random().toString(36).substr(2, 9),
+                doc = document.createElement( 'div' );
+                doc.classList.add('row');
+                doc.classList.add('document');
+                doc.innerHTML = `<img src='./imgs/doc.svg' id='${'img'+id}' alt="doc">
+                <div><h5>${acceptedFiles[0].name}</h5><p class='loading' id='${'p'+id}'>Cargando...</p></div>`;
+                files_loaded[i].appendChild(doc);
+            
+            reader.onload = e => {
+                response[pos][prop] = response[pos][prop]? [...response[pos][prop], e.target.result] : [e.target.result];
+
+                let p = this.document.querySelector(`#${'p'+id}`);
+                p.innerText = 'Cargado';
+                p.classList.remove('loading');
+
+                if(imageTypes.includes(acceptedFiles[0].type))
+                    document.querySelector(`#${'img'+id}`).src = e.target.result;
+                
+                if(response[pos][prop].length === max){
+                    inp_file.disabled = true;
+                    file_loaders[i].style.opacity = .3;
+                }
+                this.console.log(response)
+            }
+            reader.readAsDataURL(acceptedFiles[0]);
+            
         }
-        reader.readAsDataURL(acceptedFiles[0]);
+    });
 
-    };*/
 });
